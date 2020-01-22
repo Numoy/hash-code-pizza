87ae63c18b274f3aeb76d70a4a2cf50f06052728
Marvin März <marvin.maerz@protonmail.ch>
2020-01-22 18:05:01 +0100

    --

--- src/Hashcode.java
+++ src/Hashcode.java
@@ -5,10 +5,10 @@ import java.util.List;
 
 public class Hashcode {
     static int target = 100;
-    static int[] values = {4,14,15,18,29,32,36,82,95,95};
+    static int[] values = {4, 14, 15, 18, 29, 32, 36, 82, 95, 95};
     static List<Integer> selected = new ArrayList<>();
 
-    static List<Integer> solution  = new ArrayList<>();
+    static List<Integer> solution = new ArrayList<>();
     static int missedPoints = Integer.MAX_VALUE;
 
 
@@ -21,24 +21,22 @@ public class Hashcode {
     }
 
     public static void findNext(int startIndex) {
-        for (int i = startIndex; i >= 0; i-- ) {
-            if( sumOfSelected() == target )
-            {
+        for (int i = startIndex; i >= 0; i--) {
+            selected.add(Integer.valueOf(i));
+            if (sumOfSelected() == target) {
                 return;
             }
 
-            selected.add(Integer.valueOf(i));
-            if( sumOfSelected() < target ) {
-                findNext(i - 1);
-            }
-            else if ( target - sumWithoutLastElement() < missedPoints )
-                {
-                    solution = new ArrayList<>();
-                    solution.addAll(selected);
-                    missedPoints = target - sumWithoutLastElement();
-                }
 
-            selected.remove( selected.size() -1 );
+            if (sumOfSelected() < target) {
+                findNext(i - 1);
+            } else if (target - sumWithoutLastElement() < missedPoints) {
+                solution = new ArrayList<>();
+                solution.addAll(selected);
+                missedPoints = target - sumWithoutLastElement();
+            }
+
+            selected.remove(selected.size() - 1);
         }
     }
 
@@ -52,7 +50,7 @@ public class Hashcode {
 
     public static int sumWithoutLastElement() {
         int sum = 0;
-        for(int i = 0; i < selected.size() -1; i++) {
+        for (int i = 0; i < selected.size() - 1; i++) {
             sum += values[i];
         }
         return sum;
